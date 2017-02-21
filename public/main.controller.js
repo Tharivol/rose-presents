43efff9ef7002396933cc6216317d84c002bfc13
Tharivol <hughes.brewzz@gmail.com>
2017-02-20 17:41:58 -0700

    info page

--- public/main.controller.js
+++ public/main.controller.js
@@ -1,5 +1,34 @@
-angular.module("rose", [])
+angular.module("rose", ["ngRoute"])
 	.controller("roseController", Rosefunction)
+	.config(infoRoute);
+
+	infoRoute.$inject = ['$routeProvider']
+
+function infoRoute($routeProvider) {
+
+    // console.log('Router loaded')
+
+    // using the routeProvider...
+    $routeProvider
+    // chain on a route which will define the html template to insert in the ng-view when that route is hit
+    .when('/picksong',{
+        templateUrl: '/templates/picksong.html'
+    })
+    .when('/servicepackage', {
+        templateUrl: '/templates/servicepackage.html'
+    })
+    // you can also add a controller using the 'controller as' syntax which will govern that html template
+    .when('/whattowear', {
+        templateUrl: '/templates/whattowear.html'
+        
+    })
+    .when('/whoisrose', {
+        templateUrl: '/templates/whoisrose.html'
+    })
+    // and like an 'if' statement, this is our 'else' - where to go if a bogus route is entered.
+    .otherwise( { redirectTo:'/'} )
+}
+
 
 	function Rosefunction()	{
 		var rCtl= this;
@@ -17,6 +46,9 @@ angular.module("rose", [])
 		}
 
 	};
+
+
+
 // **********This is the modal for the map***********
  
 // Get the modal
@@ -27,20 +59,28 @@ var modal = document.getElementById('myModal');
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-googlemapthing = function() {
-    modal.style.display = "block";
-}
+if(modal){
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+	googlemapthing = function() {
+	    modal.style.display = "block";
+	}
 
-// // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// // When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
 }
 // **********This is the modal for the map***********
 // 
+
+
+
+
+
